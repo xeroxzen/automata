05f00ab2f8ebf558c699535022f55cb0c5f34540
Andile Mbele <andilembele020@gmail.com>
2022-11-02 20:28:37 +0200

    Doctests

--- delete_wrong_length.py
+++ delete_wrong_length.py
@@ -8,6 +8,18 @@ import os
 import sys
 
 def delete_wrong_length(directory):
+    '''
+    $ python delete_wrong_length.py /path/to/directory
+    Deleted cabfoods.co.za - wp_followup_customers_wrong_length.txt
+    Deleted cabfoods.co.za - tbtlclients_wrong_length_cleaned.txt
+    Deleted cabfoods.co.za - tbtlcustomers_wrong_length.txt
+    Deleted cabfoods.co.za - wp_dusers_wrong_length_cleaned.txt
+    Deleted cabfoods.co.za - tbtlorders_wrong_length.txt
+    Deleted cabfoods.co.za - wp_posts_wrong_length_cleaned.txt
+    Deleted cabfoods.co.za - wp_posts_wrong_length.txt
+    Deleted cabfoods.co.za - wp_comments_wrong_length_cleaned.txt
+    No 
+    '''
     for filename in os.listdir(directory):
         if 'wrong_length' in filename:
             try:
