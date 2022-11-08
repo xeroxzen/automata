a599b5c25caa45ddfc17214e450e9dc5a6e65e3d
Andile Mbele <andilembele020@gmail.com>
2022-11-08 22:37:29 +0200

    New automata script

---
+++ get_table_columns.py
@@ -0,0 +1,7 @@
+"""
+@author: Andile Jaden Mbele
+@date: 9 November 2022
+"""
+
+# Write a script that prints the headers/columns of a CSV file. The script takes a parameter of the file directory
+# Use Pandas
