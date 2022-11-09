c62711abf4db7a28ce8d888470eafb1e24555826
Andile Mbele <andilembele020@gmail.com>
2022-11-09 20:23:39 +0200

    new file, sure enough will get errors

--- get_table_columns.py
+++ get_table_columns.py
@@ -5,3 +5,24 @@
 
 # Write a script that prints the headers/columns of a CSV file. The script takes a parameter of the file directory
 # Use Pandas
+
+import pandas as pd
+import sys
+
+def get_table_columns(path):
+    '''
+    $ python3 get_table_columns.py /path/to/file.csv
+    ['id', 'name', 
+    '''
+
+    df = pd.read_csv(path)
+    count = 1
+    for each in df.columns:
+        print(count, each)
+        count +1
+
+def main():
+    get_table_columns(sys.argv[1])
+
+if __name__ == '__main__':
+    main()
