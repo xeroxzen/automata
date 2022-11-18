126bb8c45e737dc8df042d0b29be212eafcfb327
Andile Mbele <andilembele020@gmail.com>
2022-11-18 21:16:25 +0200

    delete duplicates

---
+++ delete_duplicate_rows.py
@@ -0,0 +1,27 @@
+"""
+Write a function that takes a csv file as a parameter, and deletes duplicates from the file.
+The important duplicates are the ones that have the same name, the same email address, same user id etc.
+"""
+
+import pandas as pd
+import sys
+
+def delete_duplicate_rows(csv_file):
+    duplicates_to_look_for = ['name', 'email', 'user_id', 'phone_number', 'address', 'city', 'state', 'zip_code', 'userid', 'phone', 'address1', 'city1', 'state1', 'zip1']
+    df = pd.read_csv(csv_file)
+    try:
+        for column in duplicates_to_look_for:
+            if column in df.columns:
+                df.drop_duplicates(subset=column, keep='first', inplace=True)
+        df.to_csv(csv_file, index=False)
+    except KeyError:
+        pass
+    except Exception as e:
+        print(e)    
+
+def main():
+    csv_file = sys.argv[1]
+    delete_duplicate_rows(csv_file)
+
+if __name__ == '__main__':  
+    main()
