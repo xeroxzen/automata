773d8e81f34adcb913bcb93ee63fb8dabe183586
Andile Mbele <andilembele020@gmail.com>
2022-12-21 00:20:58 +0200

    load json #11

--- json_data.py
+++ json_data.py
@@ -1,18 +1,19 @@
 import pandas as pd
 import json
+import sys
 
-def json_extractor(json_file, csv_file):
+def json_extractor(json_file):
   # Load the JSON file into a Pandas DataFrame
-  df = pd.read_json(json_file)
+  df = pd.read_json(json_file, lines=True)
 
   # Select the columns that contain useful data
   useful_data = df[['name', 'address']]
 
   # Save the useful data to a CSV file
-  useful_data.to_csv(csv_file, index=False)
+  useful_data.to_csv(json_file, index=False)
 
-def main(json_file, csv_file):
-  json_extractor(json_file, csv_file)
+def main():
+  json_extractor(sys.argv[1])
 
 if __name__ == '__main__':
-  main("/path/to/json/file.json", "/path/to/csv/file.csv")
+  main()
