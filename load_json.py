34c39d47bda67a989de5f440499592dee9245607
Andile Mbele <andilembele020@gmail.com>
2022-12-21 00:20:11 +0200

    load json #11

--- load_json.py
+++ load_json.py
@@ -1,14 +1,15 @@
 import json
 import sys
 
-def load_json(json_file):
-    with open(json_file, 'r') as f:
+def load_json():
+    with open(sys.argv[1], 'r', encoding='utf8') as f:
         data = json.load(f)
-        for person in data['_id']:
-            print(person['name'], person['address'])
+        # for person in data['_id']:
+        #     print(person['name'], person['address'])
+        print(data)
 
 def main():
-    load_json(sys.argv[1])
+    load_json()
 
 if __name__ == '__main__':
     main()
