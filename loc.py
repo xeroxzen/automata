775b46558f1b1753fdc1dc5be14aff0c8b50e391
Andile Mbele <andilembele020@gmail.com>
2023-01-31 17:16:47 +0200

    Count lines of code

---
+++ loc.py
@@ -0,0 +1,38 @@
+# Write a script that counts the lines of code in file in a directory and subdirectories.
+
+import os
+import sys
+
+def count_lines_of_code(directory):
+    # Count the number of lines of code in a directory and subdirectories.
+    count = 0
+    for dire in directory:
+        try:
+            os.chdir(dire)
+            for root, dirs, files in os.walk(dire):
+                for file in files:
+                    if file.endswith('.py'):
+                        with open(file) as f:
+                            for line in f:
+                                count += 1
+            return count
+        except FileNotFoundError:
+            print('Directory not found.')
+        except PermissionError:
+            print('Permission denied.')
+        except OSError:
+            print('OS error.')
+        except:
+            print('Unexpected error.')
+
+    print('Number of lines of code: {}'.format(count))
+
+def main():
+    '''
+    Main function.
+    '''
+    directory = sys.argv[1:]
+    print('Number of lines of code: {}'.format(count_lines_of_code(directory)))
+
+if __name__ == '__main__':
+    main()
