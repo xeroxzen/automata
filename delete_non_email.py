0a875cbb60760625cfa4ce7c6e246d0f35e34cd0
Andile Mbele <andilembele020@gmail.com>
2023-01-31 18:13:50 +0200

    Clever code  #3

---
+++ delete_non_email.py
@@ -0,0 +1,27 @@
+'''
+@author: Andile Mbele
+@program: delete_non_email.py
+'''
+import pandas as pd
+import re   
+import sys
+
+def verify_email(filename):
+    '''
+    Verify the email addresses in the "email" column. Delete any entry that does not qualify as a valid email address.
+    '''
+    df = pd.read_csv(filename, encoding='utf-8', sep=',', header=0, index_col=False, dtype=str, na_filter=False, keep_default_na=False, low_memory=False)
+    df = df[df['email'].str.contains(r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')]
+    print(df.head())
+
+    # save to csv
+    df.to_csv(filename, index=False)
+
+def main():
+    '''
+    Main function.
+    '''
+    verify_email(sys.argv[1])
+
+if __name__ == '__main__':
+    main()
