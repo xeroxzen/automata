c5f985cf9870cd80bab935274b646f3733f22051
Andile Mbele <andilembele020@gmail.com>
2023-03-24 20:10:13 +0200

    Recent changes

--- delete_non_email.py
+++ delete_non_email.py
@@ -12,7 +12,7 @@ def verify_email(filename):
     '''
     df = pd.read_csv(filename, encoding='utf-8', sep=',', header=0, index_col=False, dtype=str, na_filter=False, keep_default_na=False, low_memory=False)
     # check for multiple columns that may contain email addresses. Example: email, email2, email3, cc_email, from_email, to_email, etc.
-    df = df[df['email'].str.contains(r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')] 
+    df.loc[~df['email'].str.contains(r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')] 
     print(df.head())
 
     # save to csv
--- modify_non_url.py
+++ modify_non_url.py
@@ -15,7 +15,7 @@ def modify_url(filename):
         df = pd.read_csv(filename, encoding='utf-8', sep=',', header=0, index_col=False, dtype=str, na_filter=False, keep_default_na=False, low_memory=False)
 
         # check for columns with url addresses. URLs may not necessarily start with http or https. 
-        df.loc[~df['signature'].str.contains(r'^(https?://)?(www\.)?([a-zA-Z0-9]+\.)+[a-zA-Z]{2,}(/[^\s]*)?$', regex=True), 'signature'] = ''
+        df.loc[~df['website'].str.contains(r'^(https?://)?(www\.)?([a-zA-Z0-9]+\.)+[a-zA-Z]{2,}(/[^\s]*)?$', regex=True), 'website'] = ''
 
         print(df.head())
 
