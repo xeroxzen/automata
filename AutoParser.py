23dbd8d509327ccf4c6d1a8e4881f44368389c68
Google Jr <andilembele020@gmail.com>
2023-06-07 22:51:50 +0200

    feat: Auto parser #6

--- AutoParser.py
+++ AutoParser.py
@@ -16,9 +16,12 @@ def parse_sql_files(directory):
                     sql_file_path = os.path.join(root, file)
                     csv_file_path = os.path.splitext(sql_file_path)[0] + '.csv'
 
-                    # Call your SQL parser script here with the SQL file path and CSV file path
+                    # Call your SQL parser script here with the appropriate flag and file paths
                     # Replace 'your_parser_script.py' with the actual filename of your SQL parser script
-                    subprocess.call(['python', 'C:/Users/andile.mbele/Desktop/prescient/credscripts/SqlParserPlus.py', sql_file_path, csv_file_path])
+                    subprocess.call(['python',
+                                     'C:\\Users\\andile.mbele\\Desktop\\prescient\\credscripts\\SqlParserPlus.py',
+                                     '-s',
+                                     sql_file_path, csv_file_path])
 
         print("SQL files parsed to CSV successfully.")
     except Exception as e:
@@ -27,11 +30,16 @@ def parse_sql_files(directory):
 
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print("Please provide the directory path as a command-line argument.")
+    if len(sys.argv) < 3:
+        print("Please provide the -s flag and the directory path as command-line arguments.")
         sys.exit(1)
 
-    directory_path = sys.argv[1]  # Directory path passed as command-line argument
+    if sys.argv[1] != '-s':
+        print("Please use the -s flag to indicate the SQL to CSV extraction operation.")
+        sys.exit(1)
+
+    directory_path = sys.argv[2]  # Directory path passed as command-line argument
     parse_sql_files(directory_path)
+
 
 
