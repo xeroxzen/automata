e31b9630e45d1e626556616c2ccd8b25546dd5ee
Google Jr <andilembele020@gmail.com>
2023-06-07 22:40:25 +0200

    feat: Auto parser #5

--- AutoParser.py
+++ AutoParser.py
@@ -9,18 +9,21 @@ import sys
 
 
 def parse_sql_files(directory):
-    for root, dirs, files in os.walk(directory):
-        for file in files:
-            if file.endswith('.sql'):
-                sql_file_path = os.path.join(root, file)
-                csv_file_path = os.path.splitext(sql_file_path)[0] + '.csv'
+    try:
+        for root, dirs, files in os.walk(directory):
+            for file in files:
+                if file.endswith('.sql'):
+                    sql_file_path = os.path.join(root, file)
+                    csv_file_path = os.path.splitext(sql_file_path)[0] + '.csv'
 
-                # Call your SQL parser script here with the SQL file path and CSV file path
-                # Replace 'your_parser_script.py' with the actual filename of your SQL parser script
-                subprocess.call(['python', '~/Desktop/prescient/credscripts/SqlParserPlus.py', sql_file_path,
-                                 csv_file_path])
+                    # Call your SQL parser script here with the SQL file path and CSV file path
+                    # Replace 'your_parser_script.py' with the actual filename of your SQL parser script
+                    subprocess.call(['python', 'C:/Users/andile.mbele/Desktop/prescient/credscripts/SqlParserPlus.py', sql_file_path, csv_file_path])
 
-    print("SQL files parsed to CSV successfully.")
+        print("SQL files parsed to CSV successfully.")
+    except Exception as e:
+        print("An error occurred during SQL file parsing:")
+        print(e)
 
 
 if __name__ == '__main__':
@@ -30,4 +33,5 @@ if __name__ == '__main__':
 
     directory_path = sys.argv[1]  # Directory path passed as command-line argument
     parse_sql_files(directory_path)
+
 
