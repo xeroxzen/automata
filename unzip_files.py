56ea606dcc3242a74ee8f18f95c8fd0e09d8e15c
Google Jr <andilembele020@gmail.com>
2023-06-07 21:23:43 +0200

    feat: unzip programmatically

---
+++ unzip_files.py
@@ -0,0 +1,52 @@
+"""
+@author: Andile Jaden Mbele
+@date: 7 May 2023
+@Description: This method unzips all compressed files in nested directories.
+"""
+
+import os
+import sys
+import zipfile
+import gzip
+
+
+def unzip_files(directory):
+    total_unzipped = 0
+
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            file_path = os.path.join(root, file)
+
+            if file.endswith('.gz'):
+                try:
+                    with gzip.open(file_path, 'rb') as f_in:
+                        unzipped_file_path = os.path.splitext(file_path)[0]
+                        with open(unzipped_file_path, 'wb') as f_out:
+                            f_out.write(f_in.read())
+                        total_unzipped += 1
+                except Exception as e:
+                    print(f"Error unzipping file: {file_path}")
+                    print(f"Error message: {str(e)}")
+                    print()
+
+            elif file.endswith('.zip'):
+                try:
+                    with zipfile.ZipFile(file_path, 'r') as zip_ref:
+                        zip_ref.extractall(root)
+                        total_unzipped += len(zip_ref.namelist())
+                except Exception as e:
+                    print(f"Error unzipping file: {file_path}")
+                    print(f"Error message: {str(e)}")
+                    print()
+
+    print(f"Total files unzipped: {total_unzipped}")
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("Please provide the directory path as an argument.")
+        sys.exit(1)
+
+    directory = sys.argv[1]
+    unzip_files(directory)
+
