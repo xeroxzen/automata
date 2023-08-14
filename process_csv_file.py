0e44e39f2ba298bc670f126c50df7af922e8418b
Google Jr <andilembele020@gmail.com>
2023-08-14 18:22:20 +0200

    feat: csv deduplication #4

--- process_csv_file.py
+++ process_csv_file.py
@@ -14,23 +14,35 @@ def deduplicate_csv_files(directory_path):
       deduplicated_file_path = os.path.join(
           directory_path, file_name.replace(".csv", "_deduplicated.csv"))
 
+      # Read the CSV file into a Pandas DataFrame.
       df = pd.read_csv(original_file_path)
 
+      # Change the column name "radio11" to "gender".
       df.rename(columns={"radio11": "gender"}, inplace=True)
 
-      if "terms" in df.columns:
-        df.drop("terms", axis=1, inplace=True)
-      if "eett" in df.columns:
-        df.drop("eett", axis=1, inplace=True)
-      if "button7" in df.columns:
-        df.drop("button7", axis=1, inplace=True)
+      # Check if the columns exist.
+      try:
+        if "terms" in df.columns:
+          df.drop("terms", axis=1, inplace=True)
+        if "eett" in df.columns:
+          df.drop("eett", axis=1, inplace=True)
+        if "button7" in df.columns:
+          df.drop("button7", axis=1, inplace=True)
+      except KeyError as err:
+        print(f"Column '{err.args[0]}' does not exist in CSV file '{file_name}'.")
 
+      # Combine the "firstname" and "lastname" columns into a new column called "fullname".
       df["fullname"] = df["firstname"] + " " + df["lastname"]
 
-      df.drop(["firstname", "lastname"], axis=1, inplace=True)
+      # Drop the "firstname" and "lastname" columns.
+      try:
+        df.drop(["firstname", "lastname"], axis=1, inplace=True)
+      except KeyError as err:
+        print(f"Column '{err.args[0]}' does not exist in CSV file '{file_name}'.")
 
+      # Write the deduplicated DataFrame to a new CSV file.
       df.to_csv(deduplicated_file_path, index=False)
 
-if __name__ == "__main__":
+if name == "__main__":
   directory_path = os.getcwd()
   deduplicate_csv_files(directory_path)
