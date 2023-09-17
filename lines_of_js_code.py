d1ce7783a4a1355044cade71aaeedf7494d99e59
Google Jr <andilembele020@gmail.com>
2023-09-17 20:00:45 +0200

    count js lines of code

---
+++ lines_of_js_code.py
@@ -0,0 +1,26 @@
+import os
+import sys
+
+def count_lines_of_code(directory):
+    count = 0
+    for dir_path in directory:
+        for root, dirs, files in os.walk(dir_path):
+            for file in files:
+                if file.endswith('.js'):
+                    file_path = os.path.join(root, file)
+                    with open(file_path, 'r') as f:
+                        count += sum(1 for line in f if line.strip() and not line.startswith(('/', '/*', '*')))
+
+    return count
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python count_js_lines.py <directory>")
+        sys.exit(1)
+
+    directory = sys.argv[1:]
+    total_lines = count_lines_of_code(directory)
+    print(f'\nNumber of lines of code in .js files: {total_lines}')
+
+if __name__ == '__main__':
+    main()
