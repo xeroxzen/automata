c93c9661a5755e82e8bb38ed7df425976c3734a4
Google Jr <andilembele020@gmail.com>
2023-09-28 20:51:16 +0200

    gui approach

---
+++ merge_columns_gui.py
@@ -0,0 +1,99 @@
+import os
+import glob
+import argparse
+import pandas as pd
+import tkinter as tk
+from tkinter import filedialog, messagebox, simpledialog
+
+def merge_csv_files(directory, merge_columns=None):
+    """Merges two or more CSV files in a directory based on a common column.
+
+    Args:
+      directory: The directory containing the CSV files to merge.
+      merge_columns: A list of possible columns to merge on. If None, the
+        function will try to find a common column to merge on.
+
+    Returns:
+      A merged Pandas DataFrame.
+      type merge_columns: object
+    """
+
+    csv_files = glob.glob(os.path.join(directory, '*.csv'))
+
+    if not csv_files:
+        print("No CSV files found in the specified directory.")
+        return
+
+    if not isinstance(csv_files, list):
+        print("CSV files is not a list.")
+        return
+
+    merged_df = None
+
+    if merge_columns is None:
+        merge_columns = []
+        for csv_file in csv_files:
+            current_df = pd.read_csv(csv_file)
+            merge_columns.extend(current_df.columns)
+        merge_columns = set(merge_columns)
+
+    try:
+        data_frames = [pd.read_csv(csv_file) for csv_file in csv_files]
+
+        common_column_found = False
+
+        for merge_column in merge_columns:
+            common_rows = [df for df in data_frames if merge_column in df.columns]
+
+            if len(common_rows) >= 2 and any(len(df[df[merge_column].notna()]) > 0 for df in common_rows):
+                common_column_found = True
+                break
+
+        if not common_column_found:
+            print("No common column found or no similarity in rows to merge on.")
+            return
+
+        for csv_file, current_df in zip(csv_files, data_frames):
+            if merge_column in current_df.columns:
+                if merged_df is None:
+                    merged_df = current_df
+                else:
+                    merged_df = merged_df.merge(
+                        current_df, on=merge_column, how='outer')
+    except AttributeError as e:
+        print(f"Error: {e}")
+        return
+
+    output_filename = os.path.join(directory, 'merged_output.csv')
+    merged_df.to_csv(output_filename, index=False)
+
+    print(f"Merged data saved to {output_filename}")
+    return output_filename
+
+def select_csv_files():
+    root = tk.Tk()
+    root.withdraw()  # Hide the main window
+
+    file_paths = filedialog.askopenfilenames(
+        title="Select CSV Files to Merge", filetypes=[("CSV Files", "*.csv")]
+    )
+
+    if len(file_paths) < 2:
+        messagebox.showerror("Error", "Please select at least two CSV files for merging.")
+        return
+
+    merge_columns = simpledialog.askstring("Input", "Enter the column name to merge on:")
+
+    if merge_columns is None or merge_columns.strip() == "":
+        messagebox.showerror("Error", "Please specify a column to merge on.")
+        return
+
+    directory = os.path.dirname(file_paths[0])
+
+    output_filename = merge_csv_files(directory, [merge_columns])
+
+    if output_filename:
+        messagebox.showinfo("Success", f"Merged data saved to {output_filename}")
+
+if __name__ == "__main__":
+    select_csv_files()
