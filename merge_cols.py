a83bd844ebde5d4120508278e268c7b50130ed07
Andile Mbele <andilembele020@gmail.com>
2023-10-03 16:39:04 +0200

    feat: new approach #6

--- merge_cols.py
+++ merge_cols.py
@@ -2,12 +2,16 @@ import pandas as pd
 import tkinter as tk
 from tkinter import filedialog
 
+
 def merge_csv_files(file1, file2, merge_columns=None):
     try:
         df1 = pd.read_csv(file1)
         df2 = pd.read_csv(file2)
 
         if merge_columns is None:
+            root = tk.Tk()
+            root.withdraw()
+
             common_columns = set(df1.columns).intersection(df2.columns)
             common_columns = list(common_columns)
 
@@ -34,22 +38,38 @@ def merge_csv_files(file1, file2, merge_columns=None):
     except Exception as e:
         print(f"Error: {e}")
 
+
 def select_csv_file():
     root = tk.Tk()
     root.withdraw()  # Hide the main window
+
     file_path = filedialog.askopenfilename(title="Select a CSV file")
-    root.destroy()  # Close the tkinter window
+
+    # Return an empty string if the user cancels the file selection dialog.
+    if file_path is None:
+        file_path = ""
+
     return file_path
+
 
 if __name__ == "__main__":
     print("Select the first CSV file:")
     file1 = select_csv_file()
-    if not file1:
+
+    # Exit if the user cancels the file selection dialog.
+    if file1 == "":
         print("No file selected. Exiting.")
-    else:
-        print("Select the second CSV file:")
-        file2 = select_csv_file()
-        if not file2:
-            print("No file selected. Exiting.")
-        else:
-            merge_csv_files(file1, file2)
+        exit()
+
+    print("Select the second CSV file:")
+    file2 = select_csv_file()
+
+    # Exit if the user cancels the file selection dialog.
+    if file2 == "":
+        print("No file selected. Exiting.")
+        exit()
+
+    # Check if the file1 and file2 variables are not None before calling the merge_csv_files() function.
+    if file1 is not None and file2 is not None:
+        merge_csv_files(file1, file2)
+
