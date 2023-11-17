5c5dcd6c186cd1d4a9d04d9de9ee651a5f0837f9
Andile Mbele <andilembele020@gmail.com>
2023-11-17 19:14:48 +0200

    modified

---
+++ analyze_files.py
@@ -0,0 +1,38 @@
+import os
+import time
+import csv
+import sys
+
+def print_csv_contents(directory):
+    """
+    Print the contents of CSV files in the given directory and its subdirectories,
+    excluding files in specified folders.
+    """
+    excluded_folders = ['complete', 'sql_statements', 'unable_to_parse', 'badones', 'originals']
+
+    # Walk through the directory and its subdirectories
+    for root, dirs, files in os.walk(directory):
+        # Exclude specified folders from processing
+        dirs[:] = [d for d in dirs if d not in excluded_folders]
+
+        for file in files:
+            if file.endswith('.csv'):
+                file_path = os.path.join(root, file)
+                print(f"Analyzing contents of {file_path}:")
+
+                # Read and print the contents of the CSV file
+                with open(file_path, 'r', encoding='utf-8') as csv_file:
+                    reader = csv.reader(csv_file)
+                    for row in reader:
+                        print(row)
+                        time.sleep(1)
+
+                print()  
+
+if __name__ == '__main__':
+    # Check if the directory is provided as a command-line argument
+    if len(sys.argv) < 2:
+        print("Usage: python script.py <directory>")
+    else:
+        directory = sys.argv[1]
+        print_csv_contents(directory)
