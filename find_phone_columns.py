52c17e3513e803a44079bab95c856f40c7d3828c
Google Jr <andilembele020@gmail.com>
2023-12-13 22:11:31 +0200

    8th iteration

--- find_phone_columns.py
+++ find_phone_columns.py
@@ -16,26 +16,35 @@ def contains_separator(entry):
     return ',' in entry or ';' in entry
 
 def process_csv_file(file_path):
-    matching_entries = []
+    modified_rows = []
 
     with open(file_path, 'r', encoding="utf-8") as csv_file:
         csv_reader = csv.reader(csv_file)
         header = next(csv_reader, None)
 
         if header and has_phone_column(header):
+            new_header = header.copy()
+            for col in header:
+                if col.lower() in map(str.lower, PHONE_COLUMNS):
+                    new_header.append(f"{col}_second")
+
+            modified_rows.append(new_header)
+
             for row in csv_reader:
-                phone_column_indices = [i for i, col in enumerate(header) if col.lower() in map(str.lower, PHONE_COLUMNS)]
-                for index in phone_column_indices:
-                    if index < len(row):
+                modified_row = row.copy()
+                for index, col in enumerate(header):
+                    if col.lower() in map(str.lower, PHONE_COLUMNS):
                         phone_entry = row[index]
                         if contains_separator(phone_entry):
-                            matching_entries.append(file_path)
-                            return matching_entries
+                            entries = phone_entry.split(',' if ',' in phone_entry else ';')
+                            modified_row[index] = entries[0].strip()
+                            modified_row.append(entries[1].strip())
+                modified_rows.append(modified_row)
 
-    return matching_entries
+    return modified_rows
 
-def find_phone_columns_with_separator(directory):
-    matching_files = []
+def modify_phone_columns_with_separator(directory):
+    modified_files = {}
 
     for root, dirs, files in os.walk(directory):
         # Exclude specified directories
@@ -44,13 +53,20 @@ def find_phone_columns_with_separator(directory):
         for file in files:
             if is_csv_file(file):
                 file_path = os.path.join(root, file)
-                matching_files.extend(process_csv_file(file_path))
+                modified_files[file_path] = process_csv_file(file_path)
 
-    return matching_files
+    return modified_files
+
+def save_modified_files(modified_files):
+    for file_path, modified_rows in modified_files.items():
+        output_file_path = os.path.splitext(file_path)[0] + "_modified.csv"
+        with open(output_file_path, 'w', encoding="utf-8", newline='') as output_csv_file:
+            csv_writer = csv.writer(output_csv_file)
+            csv_writer.writerows(modified_rows)
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python find_phone_column.py <directory>")
+        print("Usage: python modify_phone_columns.py <directory>")
         sys.exit(1)
 
     target_directory = sys.argv[1]
@@ -59,11 +75,10 @@ if __name__ == "__main__":
         print(f"Error: {target_directory} is not a valid directory.")
         sys.exit(1)
 
-    matching_files = find_phone_columns_with_separator(target_directory)
+    modified_files = modify_phone_columns_with_separator(target_directory)
 
-    if matching_files:
-        print("Files with phone-related columns and entries with commas or semicolons found:")
-        for file in matching_files:
-            print(file)
+    if modified_files:
+        save_modified_files(modified_files)
+        print("Files modified successfully. Modified files saved with '_modified' suffix.")
     else:
         print("No files with phone-related columns or entries with commas or semicolons found.")
