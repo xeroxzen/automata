ab06e18157053c672831bfbc3f230298458f6724
Andile Mbele <andilembele020@gmail.com>
2024-01-19 19:22:12 +0200

    os walking

--- zerobytes_delete.py
+++ zerobytes_delete.py
@@ -2,15 +2,16 @@ import os
 import sys
 
 def delete_empty_modified_csv(directory):
-    # Iterate through files in the directory
-    for filename in os.listdir(directory):
-        if filename.endswith(".csv") and "modified" in filename.lower():
-            file_path = os.path.join(directory, filename)
+   # Iterate through files and subdirectories in the specified directory
+    for root, dirs, files in os.walk(directory):
+        for filename in files:
+            if filename.endswith(".csv") and "modified" in filename.lower():
+                file_path = os.path.join(root, filename)
 
-            # Check if the file has 0 bytes
-            if os.path.getsize(file_path) == 0:
-                print(f"Deleting {filename}")
-                os.remove(file_path)
+                # Check if the file has 0 bytes
+                if os.path.getsize(file_path) == 0:
+                    print(f"Deleting {filename}")
+                    os.remove(file_path)
 
 if __name__ == "__main__":
     target_directory = sys.argv[1]
