f393563b5cbcb844c8eb38140bc3d44af307b018
Google Jr <andilembele020@gmail.com>
2024-04-07 04:07:11 +0200

    ..changes

--- lines_of_js_code.py
+++ lines_of_js_code.py
@@ -1,17 +1,23 @@
 import os
 import sys
 
+
 def count_lines_of_code(directory):
     count = 0
     for dir_path in directory:
         for root, dirs, files in os.walk(dir_path):
             for file in files:
-                if file.endswith('.js' or '.css'):
+                if file.endswith(".js" or ".css" or ".jsx"):
                     file_path = os.path.join(root, file)
-                    with open(file_path, 'r') as f:
-                        count += sum(1 for line in f if line.strip() and not line.startswith(('/', '/*', '*')))
+                    with open(file_path, "r") as f:
+                        count += sum(
+                            1
+                            for line in f
+                            if line.strip() and not line.startswith(("/", "/*", "*"))
+                        )
 
     return count
+
 
 def main():
     if len(sys.argv) < 2:
@@ -20,7 +26,8 @@ def main():
 
     directory = sys.argv[1:]
     total_lines = count_lines_of_code(directory)
-    print(f'\nNumber of lines of code in .js files: {total_lines}')
+    print(f"\nNumber of lines of code in .js files: {total_lines}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
